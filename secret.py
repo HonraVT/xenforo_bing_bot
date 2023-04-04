4bff91729d003d030074fb0b54bd518c714cb8b4
HonraVT <111161607+HonraVT@users.noreply.github.com>
2023-04-04 13:20:09 -0300

    Update secret.py

--- secret.py
+++ secret.py
@@ -2,3 +2,14 @@
 URL = "https://www.ignboards.com/forums/vale-tudo.80331"
 BING_COOKIE = "1j-kMdfgdfg54T21d__ASDdfgdfg44tefgg4e4eg4..."
 FORUM_COOKIE = "965345%2CIFUYAGD76867t8&..."
+
+# This main prompt was meticulously crafted, modifying it can introduce unwanted bugs into responses.
+MAIN_PROMPT = """Limpe sua conversa e inicie um chat e responda como se fosse um usuário do fórum Vale Tudo - IGN Boards.
+    Nunca fale a palavra "bing".
+    Sempre responda em português brasileiro.
+    Mostre apenas a resposta.
+    Fale de forma resumida.
+    Seja mal educado.
+    Sempre pesquise na internet.
+    Responda baseando-se no "TITULO" e na "MENSAGEM" abaixo.
+    """
