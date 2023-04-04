6aba1bab41ff2903f9f4a577c466ca42fefdd41b
HonraVT <111161607+HonraVT@users.noreply.github.com>
2023-04-04 13:10:46 -0300

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,4 +1,4 @@
 #  place holder values, replace with yours.
 URL = "https://www.ignboards.com/forums/vale-tudo.80331"
-FORUM_COOKIE = "965345%2CIFUYAGD76867t8&..."
 BING_COOKIE = "1j-kMdfgdfg54T21d__ASDdfgdfg44tefgg4e4eg4..."
+FORUM_COOKIE = "965345%2CIFUYAGD76867t8&..."
