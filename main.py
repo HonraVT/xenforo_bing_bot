33b08df32173a0feb8bfaccfa80bfdd8bb6c6153
HonraVT <111161607+HonraVT@users.noreply.github.com>
2023-04-04 13:23:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,36 +7,6 @@ from forum_scraper import ForumScraper
 from secret import URL, FORUM_COOKIE, BING_COOKIE, MAIN_PROMPT
 from utils import save_conf, add_period, remove_reference_simbols
 
-"""
-Bot que usa chat do Bing para responder tópicos no forum IGN boards.
-   By Honrarias 04/04/2023
-   
-   ## Requirements
-
-- python 3.8+
-- Seguintes cookies de navegador adicionados ao arquivo ```secret.py```:
-  - Cookie ```_U``` do bing.com. Uma conta da Microsoft com acesso antecipado a https://bing.com/chat é obrigatório.
-  - Cookie ```xf_user``` de usuario do furum.
-  
-  Para obte-los:
-  
-  Acesse o bing.com e o forum pelo Chrome ou Firefox (pelo edge não funciona), faça o login e use o "inspecionar" do navegador
-  
-  ou
-  
-  instale a extensão **cookie-editor** https://cookie-editor.cgagnier.ca/ para Chrome ou Firefox:
-  - faça o login em bing.com ou o forum.
-  - Abra a extensão
-  - clique no cookie citado acima e copie o ```Value```
-
-## Configure o sub forum:
-
-Em ```secret.py``` adicione o endereço do sub fórum desejado.
-
-## Mensagens de erro:
-Erro ```NotAllowedToAccess``` significa que o cookie do bing expirou ou é inválido.
-Erro ```AuthenticationError``` significa que o cookie do forum expirou ou é inválido.
-"""
 
 fs = ForumScraper(URL, FORUM_COOKIE, warning_mgs=True)
 bing = Chatbot(cookies=[{"name": "_U", "value": BING_COOKIE}], warning_mgs=True)
